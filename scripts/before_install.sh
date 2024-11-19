d2377e940165724a927ad72b95d0e4ac966041a9
Babul Kumar Sahu <sahu15babul@gmail.com>
2024-11-19 23:38:19 +0530

    small changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-echo "Updating the instance..."
-sudo apt update -y
+echo "Updating package list..."
+sudo apt update
 
-echo "Installing MySQL and Java..."
-sudo apt install -y mysql-server openjdk-22-jdk
+echo "Installing Java..."
+sudo apt install openjdk-22-jdk -y
 
-echo "Stopping existing application..."
-sudo pkill -f 'java -jar' || true
+echo "Installing MySQL..."
+sudo apt install mysql-server -y
+sudo systemctl start mysql
+sudo systemctl enable mysql
 
 echo "Cleaning old application files..."
 sudo rm -rf /home/ubuntu/blogbackend.jar
+
+
+echo "Securing MySQL installation..."
+sudo mysql_secure_installation <<EOF
+n
+y
+y
+y
+y
+EOF
+
+echo "Configuring MySQL user and database..."
+sudo mysql -u root -p <<EOF
+CREATE DATABASE IF NOT EXISTS blog_app;
+CREATE USER IF NOT EXISTS 'babul'@'localhost' IDENTIFIED BY 'Babul@7873';
+GRANT ALL PRIVILEGES ON blog_app.* TO 'babul'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+echo "System prepared for application deployment."
+
